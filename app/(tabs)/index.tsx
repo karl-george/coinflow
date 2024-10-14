4775fba4ce226351c147927e4210cf4956782b82
Karl George <karlgeorge87@gmail.com>
2024-10-14 14:50:05 +0200

    Setup home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,15 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Colors } from '@/constants/Colors';
 
-export default function Index() {
+const Home = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ backgroundColor: Colors.background, flex: 1 }}>
+      <Text>home</Text>
     </View>
   );
-}
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
