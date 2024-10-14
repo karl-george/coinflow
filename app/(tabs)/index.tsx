c8849c8424067381daa470344dc2a0f1bb977d16
Karl George <karlgeorge87@gmail.com>
2024-10-14 15:09:58 +0200

    Add welcome section

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
 import { Colors } from '@/constants/Colors';
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const Home = () => {
+  const { top } = useSafeAreaInsets();
+
   return (
-    <View style={{ backgroundColor: Colors.background, flex: 1 }}>
-      <Text>home</Text>
+    <View style={[styles.container, { paddingTop: top + 42 }]}>
+      {/* Welcome */}
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          gap: 12,
+        }}
+      >
+        {/* Image */}
+        {/* //Todo: Fix image source when Clerk is in  */}
+        <Image
+          source={require('@/assets/images/react-logo.png')}
+          resizeMode='contain'
+          style={{ width: 80, height: 80 }}
+        />
+        {/* Text */}
+        <View>
+          <Text style={styles.name}>Hello Karl</Text>
+          <Text>Everything Crypto</Text>
+        </View>
+      </View>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.background,
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  name: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: Colors.text,
+  },
+});
