e2022ea6cf913a4ff7321cc59908c2bb50e19f9c
Karl George <karlgeorge87@gmail.com>
2024-10-17 14:42:37 +0200

    Add coin interface

--- interfaces/crypto.ts
+++ interfaces/crypto.ts
@@ -47,3 +47,28 @@ interface Platform {
   slug: string;
   token_address: string;
 }
+
+export interface Coin {
+  name: string;
+  symbol: string;
+  category: string;
+  description: string;
+  slug: string;
+  logo: string;
+  subreddit: string;
+  notice: string;
+  tags: string[];
+  'tag-names': string[];
+  'tag-groups': string[];
+  urls: string;
+  platform: null;
+  date_added: string;
+  twitter_username: string;
+  is_hidden: number;
+  date_launched: string;
+  contract_address: string[];
+  self_reported_circulating_supply: null;
+  self_reported_tags: null;
+  self_reported_market_cap: null;
+  infinite_supply: boolean;
+}
