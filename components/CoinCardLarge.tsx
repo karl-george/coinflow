62f76ac3220e63b2e7ddb06712c5ad4a2059f241
Karl George <karlgeorge87@gmail.com>
2024-10-28 10:37:03 +0100

    Add Link

--- components/CoinCardLarge.tsx
+++ components/CoinCardLarge.tsx
@@ -1,52 +1,55 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
 import { Colors } from '@/constants/Colors';
-import { Currency } from '@/interfaces/crypto';
 import { info } from '@/data/info';
+import { Currency } from '@/interfaces/crypto';
+import { Link } from 'expo-router';
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 const CoinCardLarge = ({ coin }: { coin: Currency }) => {
   return (
-    <View key={coin.id} style={styles.cardLargeBG}>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
-        <Image
-          source={{ uri: info?.[coin.id].logo }}
-          style={{ width: 64, height: 64 }}
-        />
-        <View style={{ flexDirection: 'column' }}>
+    <Link href={`/coin/${coin.id}`} key={coin.id} asChild>
+      <View style={styles.cardLargeBG}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <Image
+            source={{ uri: info?.[coin.id].logo }}
+            style={{ width: 64, height: 64 }}
+          />
+          <View style={{ flexDirection: 'column' }}>
+            <Text
+              style={{
+                fontSize: 18,
+                fontWeight: 'semibold',
+                color: Colors.text,
+              }}
+            >
+              {coin.symbol}
+            </Text>
+            <Text
+              style={{
+                fontSize: 16,
+                color: Colors.text_faded,
+              }}
+            >
+              {coin.name}
+            </Text>
+          </View>
           <Text
             style={{
               fontSize: 18,
-              fontWeight: 'semibold',
-              color: Colors.text,
+              color: Colors.accent,
             }}
           >
-            {coin.symbol}
-          </Text>
-          <Text
-            style={{
-              fontSize: 16,
-              color: Colors.text_faded,
-            }}
-          >
-            {coin.name}
+            {coin.quote.EUR.price.toFixed(0)}€
           </Text>
         </View>
-        <Text
-          style={{
-            fontSize: 18,
-            color: Colors.accent,
-          }}
-        >
-          {coin.quote.EUR.price.toFixed(0)}€
-        </Text>
       </View>
-    </View>
+    </Link>
   );
 };
 
