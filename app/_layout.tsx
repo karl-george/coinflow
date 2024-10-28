ba95cbc36ccd11ce0e2b22eba9ec9b069733bac7
Karl George <karlgeorge87@gmail.com>
2024-10-28 10:48:05 +0100

    Setup Clerk

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import { ClerkLoaded, ClerkProvider } from '@clerk/clerk-expo';
 import {
   Montserrat_400Regular,
   Montserrat_500Medium,
@@ -7,6 +8,40 @@ import {
 import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
+import * as SecureStore from 'expo-secure-store';
+
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      const item = await SecureStore.getItemAsync(key);
+      if (item) {
+        console.log(`${key} was used 🔐 \n`);
+      } else {
+        console.log('No values stored under key: ' + key);
+      }
+      return item;
+    } catch (error) {
+      console.error('SecureStore get item error: ', error);
+      await SecureStore.deleteItemAsync(key);
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env'
+  );
+}
 
 const InitialLayout = () => {
   let [fontsLoaded] = useFonts({
@@ -34,7 +69,13 @@ const InitialLayout = () => {
 };
 
 const RootLayoutNav = () => {
-  return <InitialLayout />;
+  return (
+    <ClerkProvider publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <InitialLayout />;
+      </ClerkLoaded>
+    </ClerkProvider>
+  );
 };
 
 export default RootLayoutNav;
