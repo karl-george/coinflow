1a3fd2cc9e62a1d317b0b8abc4d95ffbe3650908
Karl George <karlgeorge87@gmail.com>
2024-10-29 11:13:22 +0100

    Add temp sign out button

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
-import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo';
+import { SignedIn, SignedOut, useAuth, useUser } from '@clerk/clerk-expo';
+import { Ionicons } from '@expo/vector-icons';
 import { Link } from 'expo-router';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const Index = () => {
   const { user } = useUser();
+  const { signOut } = useAuth();
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <SignedIn>
         <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+        <TouchableOpacity onPress={() => signOut()}>
+          <Ionicons name='logo-github' size={24} />
+          <Text>Sign out</Text>
+        </TouchableOpacity>
       </SignedIn>
       <SignedOut>
         <Link href='/signin'>
