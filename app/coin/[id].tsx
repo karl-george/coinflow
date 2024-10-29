9eff62d9b44aaadec3ee9e3ae5d213fd9583f031
Karl George <karlgeorge87@gmail.com>
2024-10-29 13:38:11 +0100

    Add custom header

--- app/coin/[id].tsx
+++ app/coin/[id].tsx
@@ -1,9 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Colors } from '@/constants/Colors';
+import { info } from '@/data/info';
+import { Ionicons } from '@expo/vector-icons';
+import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
 import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const Page = () => {
+  const { id } = useLocalSearchParams();
+  const router = useRouter();
+
+  console.log(id);
+
   return (
-    <View>
+    <View style={styles.container}>
+      <Stack.Screen
+        options={{
+          headerTitle: info?.[id].name,
+          headerTitleAlign: 'center',
+          headerTintColor: Colors.text,
+          headerStyle: {
+            backgroundColor: Colors.background,
+          },
+          headerLeft: () => (
+            <View
+              style={{ flexDirection: 'row', alignItems: 'center', gap: 12 }}
+            >
+              <TouchableOpacity onPress={() => router.back()}>
+                <Ionicons name='chevron-back' size={28} color={Colors.text} />
+              </TouchableOpacity>
+              <Image
+                source={{ uri: info?.[id].logo }}
+                style={{ width: 28, height: 28 }}
+              />
+            </View>
+          ),
+        }}
+      />
       <Text>Page</Text>
     </View>
   );
@@ -11,4 +43,10 @@ const Page = () => {
 
 export default Page;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.background,
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+});
