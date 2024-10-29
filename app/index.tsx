5222d9a85a740a40e853374878f2fc5280f68fb1
Karl George <karlgeorge87@gmail.com>
2024-10-29 13:38:06 +0100

    Add temp continue button

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,21 @@
 import { SignedIn, SignedOut, useAuth, useUser } from '@clerk/clerk-expo';
 import { Ionicons } from '@expo/vector-icons';
-import { Link } from 'expo-router';
+import { Link, useRouter } from 'expo-router';
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const Index = () => {
   const { user } = useUser();
   const { signOut } = useAuth();
+  const router = useRouter();
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <SignedIn>
-        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+        <TouchableOpacity onPress={() => router.push('/(auth)/(tabs)/')}>
+          <Ionicons name='logo-github' size={24} />
+          <Text>Continue</Text>
+        </TouchableOpacity>
         <TouchableOpacity onPress={() => signOut()}>
           <Ionicons name='logo-github' size={24} />
           <Text>Sign out</Text>
