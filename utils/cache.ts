b62e2b266f7f45bfec25a799e61fadef0baea0b6
Karl George <karlgeorge87@gmail.com>
2024-10-29 11:14:03 +0100

    Remove comments

--- utils/cache.ts
+++ utils/cache.ts
@@ -7,14 +7,8 @@ const createTokenCache = (): TokenCache => {
     getToken: async (key: string) => {
       try {
         const item = await SecureStore.getItemAsync(key);
-        if (item) {
-          console.log(`${key} was used 🔐 \n`);
-        } else {
-          console.log('No values stored under key: ' + key);
-        }
         return item;
       } catch (error) {
-        console.error('SecureStore get item error: ', error);
         await SecureStore.deleteItemAsync(key);
         return null;
       }
