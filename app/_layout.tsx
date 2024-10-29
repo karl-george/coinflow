9bda7ffcacaa7de5641e0bde7c21e5187dab15a2
Karl George <karlgeorge87@gmail.com>
2024-10-29 11:13:10 +0100

    Add temp signin routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -39,6 +39,8 @@ const InitialLayout = () => {
   return (
     <Stack>
       <Stack.Screen name='index' options={{ headerShown: false }} />
+      <Stack.Screen name='signin' options={{ headerShown: false }} />
+      <Stack.Screen name='signup' options={{ headerShown: false }} />
       <Stack.Screen name='(auth)/(tabs)' options={{ headerShown: false }} />
     </Stack>
   );
