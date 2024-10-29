2338e71dc31f3ac565398b8f66f712b52402a06c
Karl George <karlgeorge87@gmail.com>
2024-10-29 14:06:51 +0100

    Add info cards

--- app/coin/[id].tsx
+++ app/coin/[id].tsx
@@ -1,3 +1,4 @@
+import Chart from '@/components/Chart';
 import { Colors } from '@/constants/Colors';
 import { info } from '@/data/info';
 import { Ionicons } from '@expo/vector-icons';
@@ -13,10 +14,12 @@ const Page = () => {
 
   return (
     <View style={styles.container}>
+      {/* Header */}
       <Stack.Screen
         options={{
           headerTitle: info?.[id].name,
           headerTitleAlign: 'center',
+          headerTitleStyle: { fontFamily: 'Montserrat_700Bold' },
           headerTintColor: Colors.text,
           headerStyle: {
             backgroundColor: Colors.background,
@@ -34,9 +37,48 @@ const Page = () => {
               />
             </View>
           ),
+          headerRight: () => (
+            <View
+              style={{ flexDirection: 'row', alignItems: 'center', gap: 12 }}
+            >
+              <TouchableOpacity onPress={() => {}}>
+                <Ionicons
+                  name='bookmark-outline'
+                  size={28}
+                  color={Colors.text_faded}
+                />
+              </TouchableOpacity>
+            </View>
+          ),
         }}
       />
-      <Text>Page</Text>
+      {/* Chart */}
+      <View style={{ marginTop: 16 }}>
+        <Chart />
+      </View>
+      {/* Info */}
+      <View style={styles.infoContainer}>
+        <View style={styles.infoCard}>
+          <View style={styles.textInfoRow}>
+            <Text style={styles.textInfo}>Open</Text>
+            <Text style={styles.textInfo}>123456</Text>
+          </View>
+          <View style={styles.textInfoRow}>
+            <Text style={styles.textInfo}>Close</Text>
+            <Text style={styles.textInfo}>123456</Text>
+          </View>
+        </View>
+        <View style={styles.infoCard}>
+          <View style={styles.textInfoRow}>
+            <Text style={styles.textInfo}>High</Text>
+            <Text style={styles.textInfo}>123456</Text>
+          </View>
+          <View style={styles.textInfoRow}>
+            <Text style={styles.textInfo}>Low</Text>
+            <Text style={styles.textInfo}>123456</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
@@ -48,5 +90,30 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.background,
     flex: 1,
     paddingHorizontal: 16,
+  },
+  infoContainer: {
+    marginTop: 16,
+    flexDirection: 'row',
+    gap: 12,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  infoCard: {
+    backgroundColor: Colors.card_light,
+    borderRadius: 12,
+    flex: 1,
+    paddingVertical: 16,
+    paddingHorizontal: 20,
+    gap: 12,
+  },
+  textInfoRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  textInfo: {
+    fontSize: 16,
+    color: Colors.text,
+    fontFamily: 'Montserrat_600SemiBold',
   },
 });
