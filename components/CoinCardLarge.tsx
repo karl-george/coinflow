4530d684fd1fac14457a4ab80b8f734fc6a22f20
Karl George <karlgeorge87@gmail.com>
2024-11-01 09:53:01 +0100

    Add sparkline

--- components/CoinCardLarge.tsx
+++ components/CoinCardLarge.tsx
@@ -7,74 +7,73 @@ import { SvgUri } from 'react-native-svg';
 
 const CoinCardLarge = ({ coin }: { coin: Coin }) => {
   return (
-    <Link href={`/coin/${coin.item.coin_id}`} key={coin.item.coin_id} asChild>
-      <View style={styles.cardLargeBG}>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}
-        >
-          {/* Image and name */}
-          <View style={{ flexDirection: 'row', gap: 10, alignItems: 'center' }}>
-            <Image
-              source={{ uri: coin.item.small }}
-              style={{ width: 64, height: 64, borderRadius: 100 }}
-            />
-            <View style={{ flexDirection: 'column' }}>
-              <Text
-                style={{
-                  fontSize: 18,
-                  fontWeight: 'semibold',
-                  color: Colors.text,
-                }}
-              >
-                {coin.item.symbol}
-              </Text>
-              <Text
-                style={{
-                  fontSize: 16,
-                  color: Colors.text_faded,
-                  maxWidth: 78,
-                }}
-              >
-                {coin.item.name}
-              </Text>
-            </View>
-          </View>
+    <View style={styles.cardLargeBG}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        {/* Image and name */}
+        <View style={{ flexDirection: 'row', gap: 10, alignItems: 'center' }}>
+          <Image
+            source={{ uri: coin.item.small }}
+            style={{ width: 64, height: 64, borderRadius: 100 }}
+          />
 
-          {/* Sparkline */}
-          <View>
-            <SvgUri uri={coin.item.data.sparkline} width={64} height={24} />
-          </View>
-
-          {/* Price */}
           <View style={{ flexDirection: 'column' }}>
             <Text
               style={{
                 fontSize: 18,
+                fontWeight: 'semibold',
                 color: Colors.text,
               }}
             >
-              €{coin.item.data.price.toFixed(2)}
+              {coin.item.symbol}
             </Text>
             <Text
-              style={[
-                { fontSize: 14, color: Colors.accent, alignSelf: 'flex-end' },
-                parseFloat(
-                  coin.item.data.price_change_percentage_24h.eur.toFixed(2)
-                ) < 0
-                  ? { color: '#D21F3C' }
-                  : { color: Colors.accent },
-              ]}
+              style={{
+                fontSize: 16,
+                color: Colors.text_faded,
+                maxWidth: 78,
+              }}
             >
-              {coin.item.data.price_change_percentage_24h.eur.toFixed(2)}%
+              {coin.item.name}
             </Text>
           </View>
         </View>
+
+        {/* Sparkline */}
+        <View>
+          <SvgUri uri={coin.item.data.sparkline} width={64} height={24} />
+        </View>
+
+        {/* Price */}
+        <View style={{ flexDirection: 'column' }}>
+          <Text
+            style={{
+              fontSize: 18,
+              color: Colors.text,
+            }}
+          >
+            €{coin.item.data.price.toFixed(2)}
+          </Text>
+          <Text
+            style={[
+              { fontSize: 14, color: Colors.accent, alignSelf: 'flex-end' },
+              parseFloat(
+                coin.item.data.price_change_percentage_24h.eur.toFixed(2)
+              ) < 0
+                ? { color: '#D21F3C' }
+                : { color: Colors.accent },
+            ]}
+          >
+            {coin.item.data.price_change_percentage_24h.eur.toFixed(2)}%
+          </Text>
+        </View>
       </View>
-    </Link>
+    </View>
   );
 };
 
