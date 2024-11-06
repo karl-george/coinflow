9267626aba7c13dd814f7f0200003cb3cd64b95c
Karl George <karlgeorge87@gmail.com>
2024-11-06 12:14:14 +0100

    Add save screen styling

--- app/(auth)/(tabs)/save/save.tsx
+++ app/(auth)/(tabs)/save/save.tsx
@@ -1,14 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Colors } from '@/constants/Colors';
 import React from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const Save = () => {
+  const { top, bottom } = useSafeAreaInsets();
+
   return (
-    <View>
-      <Text>Save</Text>
-    </View>
+    <ScrollView style={[styles.container]}>
+      <View style={{ marginTop: top + 42 }}>
+        <Text style={styles.title}>Saved Coins</Text>
+      </View>
+    </ScrollView>
   );
 };
 
 export default Save;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.background,
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  title: {
+    marginTop: 16,
+    fontWeight: 'semibold',
+    fontSize: 20,
+    color: Colors.text,
+    fontFamily: 'Montserrat_600SemiBold',
+  },
+});
