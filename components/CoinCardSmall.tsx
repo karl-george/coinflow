ee8edf040ade470ff1c9eb9c044e07da601a0316
Karl George <karlgeorge87@gmail.com>
2024-11-06 11:59:17 +0100

    Add styling

--- components/CoinCardSmall.tsx
+++ components/CoinCardSmall.tsx
@@ -1,6 +1,7 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
 import { Colors } from '@/constants/Colors';
+import { Link } from 'expo-router';
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 type Props = {
   coin: {
@@ -16,9 +17,17 @@ type Props = {
 
 const CoinCardSmall = ({ coin }: Props) => {
   return (
-    <View style={styles.coinCardSmall}>
-      <Image source={{ uri: coin.thumb }} style={{ width: 64, height: 64 }} />
-    </View>
+    <Link href={`/coin/${coin.id}`}>
+      <View style={styles.coinCardSmall}>
+        <Image
+          source={{ uri: coin.thumb }}
+          style={{ width: 64, height: 64, borderRadius: 100 }}
+        />
+        <Text style={styles.coinCardText} numberOfLines={2}>
+          {coin.name}
+        </Text>
+      </View>
+    </Link>
   );
 };
 
@@ -27,10 +36,18 @@ export default CoinCardSmall;
 const styles = StyleSheet.create({
   coinCardSmall: {
     width: 84,
-    height: 84,
+    height: 120,
     backgroundColor: Colors.card_light,
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 12,
+    gap: 8,
+    paddingHorizontal: 2,
+  },
+  coinCardText: {
+    color: Colors.text,
+    textAlign: 'center',
+    overflow: 'hidden',
+    flexWrap: 'wrap',
   },
 });
