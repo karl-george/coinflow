5b616fa70189c767f8828d7b79c4b837076442a4
Karl George <karlgeorge87@gmail.com>
2024-11-06 11:38:06 +0100

    Add search input

--- app/(auth)/(tabs)/search/search.tsx
+++ app/(auth)/(tabs)/search/search.tsx
@@ -1,14 +1,62 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { Colors } from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const Search = () => {
+  const { top } = useSafeAreaInsets();
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
-    <View>
-      <Text>Search</Text>
+    <View style={styles.container}>
+      {/* Search Input */}
+      <View style={[{ marginTop: top + 42 }, styles.inputContainer]}>
+        <Ionicons name='search' size={24} color={Colors.text_faded} />
+        <TextInput
+          placeholder='Search for coins...'
+          placeholderTextColor={Colors.text_faded}
+          style={styles.inputText}
+          value={searchTerm}
+          onChangeText={setSearchTerm}
+        />
+      </View>
+      {/* Search Title */}
+      <View>
+        <Text style={styles.searchTitle}>{searchTerm}</Text>
+      </View>
     </View>
   );
 };
 
 export default Search;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.background,
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    borderWidth: 1,
+    borderColor: Colors.card_light,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    backgroundColor: Colors.card_dark,
+    borderRadius: 8,
+    flexDirection: 'row',
+    gap: 12,
+    alignItems: 'center',
+  },
+  inputText: {
+    color: Colors.text,
+    fontSize: 16,
+  },
+  searchTitle: {
+    marginTop: 16,
+    fontWeight: 'semibold',
+    fontSize: 20,
+    color: Colors.text,
+    fontFamily: 'Montserrat_600SemiBold',
+  },
+});
