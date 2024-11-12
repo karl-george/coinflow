c5262c93a0f9d0a798371b97444c6f9343594b14
Karl George <karlgeorge87@gmail.com>
2024-11-12 14:39:02 +0100

    Remove signed in components

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,19 @@
-import { SignedIn, SignedOut, useAuth, useUser } from '@clerk/clerk-expo';
-import { Ionicons } from '@expo/vector-icons';
-import { Link, useRouter } from 'expo-router';
+import { Colors } from '@/constants/Colors';
+import { SignedOut } from '@clerk/clerk-expo';
+import { Link } from 'expo-router';
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const Index = () => {
-  const { user } = useUser();
-  const { signOut } = useAuth();
-  const router = useRouter();
-
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <SignedIn>
-        <TouchableOpacity onPress={() => router.push('/(auth)/(tabs)/')}>
-          <Ionicons name='logo-github' size={24} />
-          <Text>Continue</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => signOut()}>
-          <Ionicons name='logo-github' size={24} />
-          <Text>Sign out</Text>
-        </TouchableOpacity>
-      </SignedIn>
+    <View
+      style={{
+        backgroundColor: Colors.background,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
       <SignedOut>
         <Link href='/signin'>
           <Text>Sign In</Text>
