3fd02917280fdc24f17a0df79b5cc4b01db50ed6
Karl George <karlgeorge87@gmail.com>
2024-11-15 13:20:13 +0100

    Adjust card_dark color

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,7 +1,7 @@
 export const Colors = {
   background: '#141414',
   card_light: '#292929',
-  card_dark: '#1E1E1E',
+  card_dark: '#202020',
   accent: '#67E7BB',
   text_faded: '#646464',
   text: '#FEFEFE',
